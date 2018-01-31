864cbafbfb50e44f9cf020e06254de442cd6c2ba
Ghaith Dalla-Ali <ghaith.dalla-ali@solace.com>
2018-01-31 10:06:15 -0500

    SOL-1253: As a software developer/architect I have access to a SolaceProducts QuickStart Project that supports my efforts to use the latest Solace Multi-cloud and Docker Improvement VMR features in AWS (#20)
    * Modified the install-solace script to accept any arbitrary load. Added a log file, /tmp/install-solace.log, to log what's going on during installation
    
    * Adding cloudwatch host and container logging support
    
    * - Adding load balancer support with health-check (using Classic ELBs)
    - Adding new instance type (m4.4xlarge)
    - Adding new container cli/bash aliases
    
    * For 'Solace Docker URL', it now accepts solace redirect links of VMR loads as well as loads hosted elsewhere
    
    * -Adding support for Secrets: The VMR now uses Secret files for sensitive configurations
    -Added more listeners to the ELB
    -Updated list of valid AWS instance types
    
    * Added swap setup for smaller instance-types. Removed debug logging and cleaned up the code. Minor fixes to documentation.
    
    * Adding carriage return to the end of password file. Exposing port 8080 to ELB. Fixing semp query password logic
    
    * Updating the Solace-AWS-HA-Prod-3AZ.png image with the new health-check-enabled ELB
    
    * Added doc on how to access the VMR's CLI
    
    * Using relative paths for images in README.md
    
    * Updating links to point to the SolaceProducts project instead of SolaceLabs. Updating referenced default S3 bucket name and paths.
    
    * Added/updated documentation around VMR logging, min requirements, an
    d admin access. Removed larger instance-types choices from MonitorNodeInstanceType. Updated VMR log stream name in CloudWatch. Updated default values of template CIDRs
    
    * Adding CloudWatch_logging.png image to documentation
    
    * Added the health-check port (5550) to the SolaceInternalSecurityGroupMember. Added the ELB to the same secgrp
    
    * Removing the health-check port from MessageRouterSecurityGroup
    
    * Adding more documentation around specifying AZs
    
    * More docs around number of AZs
    
    * Adding docs around S3 bucket usage
    
    * Added link to docker logging documentation
    
    * Removing SSL ports from ELB, exposing rest of ports to relevant sec grps, fix for static Solace download links
    
    * Removing m5 instance types until official support
    
    * Adding docs around using 'New VPC' versus 'Existing VPC'

--- scripts/install-solace.sh
+++ scripts/install-solace.sh
@@ -27,10 +27,13 @@ disk_size=""
 volume=""
 DEBUG="-vvvv"
 is_primary="false"
+logging_format=""
+logging_group=""
+logging_stream=""
 
 verbose=0
 
-while getopts "c:d:p:s:u:v:" opt; do
+while getopts "c:d:p:s:u:v:f:g:r:" opt; do
     case "$opt" in
     c)  config_file=$OPTARG
         ;;
@@ -44,6 +47,12 @@ while getopts "c:d:p:s:u:v:" opt; do
         ;;
     v)  volume=$OPTARG
         ;;
+    f) logging_format=$OPTARG
+        ;;
+    g) logging_group=$OPTARG
+        ;;
+    r) logging_stream=$OPTARG
+        ;;
     esac
 done
 
@@ -51,24 +60,34 @@ shift $((OPTIND-1))
 [ "$1" = "--" ] && shift
 
 verbose=1
-echo "config_file=$config_file ,solace_directory=$solace_directory ,admin_password_file=$admin_password_file, \
-      solace_url=$solace_url ,disk_size=$disk_size , volume=$volume ,Leftovers: $@"
+echo "config_file=$config_file , solace_directory=$solace_directory , admin_password_file=$admin_password_file , \
+      solace_url=$solace_url , disk_size=$disk_size , volume=$volume , logging_format=$logging_format , \
+      logging_group=$logging_group , logging_stream=$logging_stream , Leftovers: $@"
 
 export admin_password=`cat ${admin_password_file}`
-rm ${admin_password_file}
 mkdir $solace_directory
 cd $solace_directory
 echo "`date` INFO: Configure VMRs Started"
 
 echo "`date` INFO: check to make sure we have a complete load"
-wget -O ${solace_directory}/solos.info -nv  https://products.solace.com/download/VMR_DOCKER_EVAL_MD5
+if [[ ${solace_url} == *"em.solace.com"* ]]; then
+    # a redirect link provided by solace
+    wget -O ${solace_directory}/solos.info -nv  https://products.solace.com/download/VMR_DOCKER_EVAL_MD5
+else
+    # an already-existing load (plus its md5 file) hosted somewhere else (e.g. in an s3 bucket)
+    wget -O ${solace_directory}/solos.info -nv  ${solace_url}.md5
+fi
 IFS=' ' read -ra SOLOS_INFO <<< `cat ${solace_directory}/solos.info`
 MD5_SUM=${SOLOS_INFO[0]}
 SolOS_LOAD=${SOLOS_INFO[1]}
 echo "`date` INFO: Reference md5sum is: ${MD5_SUM}"
 
 wget -q -O solace-redirect ${solace_url}
-REAL_LINK=`egrep -o "https://[a-zA-Z0-9\.\/\_\?\=%]*" ${solace_directory}/solace-redirect`
+if [[ ${solace_url} == *"em.solace.com"* ]]; then
+    REAL_LINK=`egrep -o "https://[a-zA-Z0-9\.\/\_\?\=%]*" ${solace_directory}/solace-redirect`
+else
+    REAL_LINK=${solace_url}
+fi
 wget -q -O  ${solace_directory}/${SolOS_LOAD} ${REAL_LINK}
 cd ${solace_directory}
 LOCAL_OS_INFO=`md5sum ${SolOS_LOAD}`
@@ -80,12 +99,26 @@ else
     echo "`date` INFO: Successfully downloaded ${SolOS_LOAD}"
 fi
 
+# Memory setup for when using smaller instance-types
+echo "`date` INFO: Set up swap for < 6GB machines"
+MEM_SIZE=`cat /proc/meminfo | grep MemTotal | tr -dc '0-9'`
+if [ ${MEM_SIZE} -lt 6087960 ]; then
+ echo "`date` WARN: Not enough memory: ${MEM_SIZE} Creating 2GB Swap space"
+ mkdir /var/lib/solace
+ dd if=/dev/zero of=/var/lib/solace/swap count=2048 bs=1MiB
+ mkswap -f /var/lib/solace/swap
+ chmod 0600 /var/lib/solace/swap
+ swapon -f /var/lib/solace/swap
+ grep -q 'solace\/swap' /etc/fstab || sudo sh -c 'echo "/var/lib/solace/swap none swap sw 0 0" >> /etc/fstab'
+else
+  echo "`date` INFO: Memory size is ${MEM_SIZE}"
+fi
 
 # Make sure Docker is actually up
 docker_running=""
 loop_guard=6
 loop_count=0
-while [ ${loop_count} != ${loop_guard} ]; do 
+while [ ${loop_count} != ${loop_guard} ]; do
     sleep 10
     docker_running=`service docker status | grep -o running`
     if [ ${docker_running} != "running" ]; then
@@ -97,6 +130,7 @@ while [ ${loop_count} != ${loop_guard} ]; do
     fi
 done
 
+echo "`date` INFO: Executing 'docker load'"
 docker load -i ${solace_directory}/${SolOS_LOAD}
 
 export VMR_VERSION=`docker images | grep solace | awk '{print $3}'`
@@ -107,42 +141,42 @@ host_name=`hostname`
 host_info=`grep ${host_name} ${config_file}`
 local_role=`echo $host_info | grep -o -E 'Monitor|MessageRouterPrimary|MessageRouterBackup'`
 
-#Get the IP addressed for node
+# Get the IP addressed for node
 for role in Monitor MessageRouterPrimary MessageRouterBackup
-do 
+do
     role_info=`grep ${role} ${config_file}`
     role_name=${role_info%% *}
     role_ip=`echo ${role_name} | cut -c 4- | tr "-" .`
-    case $role in  
+    case $role in
         Monitor )
             MONITOR_IP=${role_ip}
-            ;; 
-        MessageRouterPrimary ) 
+            ;;
+        MessageRouterPrimary )
             PRIMARY_IP=${role_ip}
-            ;; 
-        MessageRouterBackup ) 
+            ;;
+        MessageRouterBackup )
             BACKUP_IP=${role_ip}
-            ;; 
+            ;;
     esac
 done
 
-case $local_role in  
+case $local_role in
     Monitor )
         NODE_TYPE="monitoring"
         ROUTER_NAME="monitor"
         REDUNDANCY_CFG=""
-        ;; 
-    MessageRouterPrimary ) 
+        ;;
+    MessageRouterPrimary )
         NODE_TYPE="message_routing"
         ROUTER_NAME="primary"
         REDUNDANCY_CFG="--env redundancy_matelink_connectvia=${BACKUP_IP} --env redundancy_activestandbyrole=primary --env configsync_enable=yes"
         is_primary="true"
-        ;; 
-    MessageRouterBackup ) 
+        ;;
+    MessageRouterBackup )
         NODE_TYPE="message_routing"
         ROUTER_NAME="backup"
         REDUNDANCY_CFG="--env redundancy_matelink_connectvia=${PRIMARY_IP} --env redundancy_activestandbyrole=backup --env configsync_enable=yes"
-        ;; 
+        ;;
 esac
 
 if [ $disk_size == "0" ]; then
@@ -166,27 +200,40 @@ else
     SPOOL_MOUNT="-v /opt/vmr:/usr/sw/internalSpool -v /opt/vmr:/usr/sw/adb -v /opt/vmr:/usr/sw/internalSpool/softAdb"
 fi
 
-# Start up the SolOS docer instance with HA config keys
+# Start up the SolOS docker instance with HA config keys
+echo "`date` INFO: Executing 'docker create'"
 docker create \
    --uts=host \
    --shm-size 2g \
    --ulimit core=-1 \
    --ulimit memlock=-1 \
    --ulimit nofile=2448:1048576 \
-   --cap-add=IPC_LOCK \
-   --cap-add=SYS_NICE \
    --net=host \
    --restart=always \
    -v jail:/usr/sw/jail \
    -v var:/usr/sw/var \
+   -v /mnt/vmr/secrets:/run/secrets \
    ${SPOOL_MOUNT} \
+   --log-driver=awslogs \
+   --log-opt awslogs-group=${logging_group} \
+   --log-opt awslogs-stream=${logging_stream} \
+   --env "logging_debug_output=all" \
+   --env "logging_debug_format=${logging_format}" \
+   --env "logging_command_output=all" \
+   --env "logging_command_format=${logging_format}" \
+   --env "logging_system_output=all" \
+   --env "logging_system_format=${logging_format}" \
+   --env "logging_event_output=all" \
+   --env "logging_event_format=${logging_format}" \
+   --env "logging_kernel_output=all" \
+   --env "logging_kernel_format=${logging_format}" \
    --env "nodetype=${NODE_TYPE}" \
    --env "routername=${ROUTER_NAME}" \
    --env "username_admin_globalaccesslevel=admin" \
-   --env "username_admin_password=${admin_password}" \
+   --env "username_admin_passwordfilepath=$(basename ${admin_password_file})" \
    --env "service_ssh_port=2222" \
    ${REDUNDANCY_CFG} \
-   --env "redundancy_group_password=${admin_password}" \
+   --env "redundancy_group_passwordfilepath=$(basename ${admin_password_file})" \
    --env "redundancy_enable=yes" \
    --env "redundancy_group_node_primary_nodetype=message_routing" \
    --env "redundancy_group_node_primary_connectvia=${PRIMARY_IP}" \
@@ -196,25 +243,51 @@ docker create \
    --env "redundancy_group_node_monitor_connectvia=${MONITOR_IP}" \
    --name=solace ${VMR_VERSION}
 
-
-#Start the solace service and enable it at system start up.
+# Start the solace service and enable it at system start up.
 chkconfig --add solace-vmr
+echo "`date` INFO: Starting Solace service"
 service solace-vmr start
 
+# Poll the VMR SEMP port until it is Up
 loop_guard=30
 pause=10
 count=0
+echo "`date` INFO: Wait for the VMR SEMP service to be enabled"
+while [ ${count} -lt ${loop_guard} ]; do
+  online_results=`/tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
+    -q "<rpc semp-version='soltr/8_7VMR'><show><service/></show></rpc>" \
+    -v "/rpc-reply/rpc/show/service/services/service[name='SEMP']/enabled[text()]"`
+  is_vmr_up=`echo ${online_results} | jq '.valueSearchResult' -`
+  echo "`date` INFO: SEMP service 'enabled' status is: ${is_vmr_up}"
+  run_time=$((${count} * ${pause}))
+  if [ "${is_vmr_up}" = "\"true\"" ]; then
+      echo "`date` INFO: VMR SEMP service is up, after ${run_time} seconds"
+      break
+  fi
+  ((count++))
+  echo "`date` INFO: Waited ${run_time} seconds, VMR SEMP service not yet up"
+  sleep ${pause}
+done
 
+# Remove all VMR Secrets from the host; at this point, the VMR should have come up
+# and it won't be needing those files anymore
+rm ${admin_password_file}
 
+if [ ${count} -eq ${loop_guard} ]; then
+  echo "`date` ERROR: Solace VMR SEMP service never came up" | tee /dev/stderr
+  exit 1
+fi
+
+# Poll the redundancy status on the Primary VMR
 loop_guard=30
 pause=10
 count=0
 mate_active_check=""
-echo "`date` INFO: Wait for Primary to be 'Local Active' or 'Mate Active'"
 if [ "${is_primary}" = "true" ]; then
-  while [ ${count} -lt ${loop_guard} ]; do 
-    online_results=`./semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
-         -q "<rpc semp-version='soltr/8_5VMR'><show><redundancy><detail/></redundancy></show></rpc>" \
+  echo "`date` INFO: Wait for Primary to be 'Local Active' or 'Mate Active'"
+  while [ ${count} -lt ${loop_guard} ]; do
+    online_results=`/tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
+         -q "<rpc semp-version='soltr/8_7VMR'><show><redundancy><detail/></redundancy></show></rpc>" \
          -v "/rpc-reply/rpc/show/redundancy/virtual-routers/primary/status/activity[text()]"`
 
     local_activity=`echo ${online_results} | jq '.valueSearchResult' -`
@@ -240,16 +313,16 @@ if [ "${is_primary}" = "true" ]; then
 
   if [ ${count} -eq ${loop_guard} ]; then
     echo "`date` ERROR: Solace redundancy group never came up" | tee /dev/stderr
-    exit 1 
+    exit 1
   fi
 
-  loop_guard=30
+  loop_guard=45
   pause=10
   count=0
   echo "`date` INFO: Wait for Backup to be 'Active' or 'Standby'"
-  while [ ${count} -lt ${loop_guard} ]; do 
-    online_results=`./semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
-         -q "<rpc semp-version='soltr/8_5VMR'><show><redundancy><detail/></redundancy></show></rpc>" \
+  while [ ${count} -lt ${loop_guard} ]; do
+    online_results=`/tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
+         -q "<rpc semp-version='soltr/8_7VMR'><show><redundancy><detail/></redundancy></show></rpc>" \
          -v "/rpc-reply/rpc/show/redundancy/virtual-routers/primary/status/detail/priority-reported-by-mate/summary[text()]"`
 
     mate_activity=`echo ${online_results} | jq '.valueSearchResult' -`
@@ -275,11 +348,11 @@ if [ "${is_primary}" = "true" ]; then
 
   if [ ${count} -eq ${loop_guard} ]; then
     echo "`date` ERROR: Solace redundancy group never came up" | tee /dev/stderr
-    exit 1 
+    exit 1
   fi
 
- /tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
-         -q "<rpc semp-version='soltr/8_5VMR'><admin><config-sync><assert-master><router/></assert-master></config-sync></admin></rpc>"
- /tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
-         -q "<rpc semp-version='soltr/8_5VMR'><admin><config-sync><assert-master><vpn-name>default</vpn-name></assert-master></config-sync></admin></rpc>"
+  /tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
+    -q "<rpc semp-version='soltr/8_7VMR'><admin><config-sync><assert-master><router/></assert-master></config-sync></admin></rpc>"
+  /tmp/semp_query.sh -n admin -p ${admin_password} -u http://localhost:8080/SEMP \
+    -q "<rpc semp-version='soltr/8_7VMR'><admin><config-sync><assert-master><vpn-name>default</vpn-name></assert-master></config-sync></admin></rpc>"
 fi
